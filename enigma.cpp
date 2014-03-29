6f57ba35d0a001b750ba70655f437d657851df81
synergiance <lordmicro@gmail.com>
2014-03-29 17:09:47 -0400

    First traces of code

---
+++ enigma.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <signal.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Nice try, it aint coded yet.  Try again after I wrote some ACTUAL code\n";
+    return 0;
+}
